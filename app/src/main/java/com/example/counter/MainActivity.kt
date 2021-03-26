4c427b3a6cc57cfe51a2a43cd57c654b1187560f
s1081703 <s1081703@gm.pu.edu.tw>
2021-03-26 16:00:47 +0800

    加一、加二、歸零、樂透、圖片

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -2,49 +2,44 @@ package com.example.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity()/*, View.OnClickListener, View.OnLongClickListener, View.OnTouchListener */{
+class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClickListener{
 
     var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //txv.setOnClickListener(this)
-        //btn.setOnClickListener(this)
-        //txvvb.setOnTouchListener(this)
+        txv.setOnClickListener(this)
+        btn.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        btnn.setOnClickListener(this)
+        txv.setOnLongClickListener(this)
+        //txv.setOnTouchListener(this)
         txv.text = counter.toString()
-
-        /*btnHappy.setOnClickListener(object : View.OnClickListener{
+        btnHappy.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
                 counter= (1..100).random()
                 txv.text = counter.toString()
             }
-        })*/
+        })
 
     }
 
-    fun Add(v:View){
-        //if(v.id == R.id.btn){
-        if(v == btn){
+
+    override fun onClick(view: View)  {
+        if (view == txv || view == btn){
             counter++
-        }
-        else{
-            counter+=2
-        }
-        txv.text = counter.toString()
-    }
-    /*override fun onClick(view: View) {
-        if (view.id.equals(R.id.txv)){
-            counter++
+        }else if (view == button6){
+            counter += 2
         }
         else{
             counter = 0
         }
+
         txv.text = counter.toString()
     }
 
@@ -53,24 +48,4 @@ class MainActivity : AppCompatActivity()/*, View.OnClickListener, View.OnLongCli
         txv.text = counter.toString()
         return true
     }
-
-    override fun onTouch(p0: View?, event: MotionEvent): Boolean {
-        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        if (event.action == MotionEvent.ACTION_DOWN){
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                //Build.VERSION.SDK_INT >= 26, New vibrate method for API Level 26 or higher
-                vibrator.vibrate(VibrationEffect.createOneShot(5000, VibrationEffect.DEFAULT_AMPLITUDE))
-            } else {
-                vibrator.vibrate(5000)
-            }
-        }
-        else{
-            vibrator.cancel()
-        }
-        return true
-    }*/
-
-
-
-
 }
